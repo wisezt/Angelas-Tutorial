7767a48b61c4f147b74b65f3ffe5ea358f6e79ba
Ting <wisezt@hotmail.com>
2019-09-29 14:09:07 -0500

    S18 response & request

--- Section_18_Expressjs/server.js
+++ Section_18_Expressjs/server.js
@@ -4,7 +4,21 @@ const express = require("express");
 
 const app = express(); // app is used commonly and easier for other people to understhand
 
+
+app.get("/", function(request, response){ // rep, res
+
+  //console.log(request);
+  //console.log(response);
+  response.send("<h1>Hello World</h1>");
+
+
+
+});
+
+
+
+
 app.listen(3000, function(){
   console.log("Server Started on port 3000");
-  
+
 });
