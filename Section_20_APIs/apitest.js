7fe8770a79bd82b3da62fb97b86db019522fe12a
Ting <wisezt@hotmail.com>
2019-09-30 16:06:58 -0500

    S20 res.write() & res.send()

--- Section_20_APIs/apitest.js
+++ Section_20_APIs/apitest.js
@@ -1,13 +1,46 @@
 // jshint esversion: 6
 
 const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
+const request = require("request");
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
+
+
+app.post("/", function(req, res){
+
+  console.log(req.body.crypto);
+
+  var crypto =req.body.crypto;
+  var fiat = req.body.fiat;
+  var urlBase = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+  var url = urlBase + crypto + fiat;
+
+
+
+
+
+
+  request(url, function(error, response, body){
+
+    var data = JSON.parse(body);
+    var price = data.last;
+  var currentDate = data.display_timestamp;
+    res.write("<h1>The current data is " + currentDate + "</h1>");
+    res.write("<h1>This is the price convert from " + crypto + " to " + fiat + " : "  + price + "</h1>");
+    res.send();
+  });
+
+
+});
+
+
 
 
 
