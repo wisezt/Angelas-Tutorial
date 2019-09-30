343cb787e6ddb7dc1e9e85fa976bd632a0020508
Ting <wisezt@hotmail.com>
2019-09-29 20:30:33 -0500

    S18 BMI Calculator

--- Section_18_Express_Calculator/calculator.js
+++ Section_18_Express_Calculator/calculator.js
@@ -4,15 +4,50 @@ const express = require("express");
 
 const app = express(); // app is used commonly and easier for other people to understhand
 
+const bodyParser = require("body-parser");
 
-app.get("/", function(req, res){
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-  res.send("<h1>hellow world</h1>");
+
+
+app.get("/", function(req, res) {
+
+  res.sendFile(__dirname + "/index.html");
 
 })
 
 
-app.listen(3000, function(){
+
+
+app.post("/", function(req, res) {
+
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var result = num1 + num2;
+
+  res.send("The Result is " + result);
+
+});
+
+app.get("/bmiCalculator", function(req, res) {
+
+  res.sendFile(__dirname + "/bmiCalculator.html");
+
+})
+
+
+app.post("/bmiCalculator", function(req, res) {
+  var height = Number(req.body.height);
+  var weight = Number(req.body.weight);
+  var result = weight / Math.pow(height, 2);
+
+  res.send("Your BMI is " + result);
+});
+
+
+app.listen(3000, function() {
   console.log("Server Started on port 3000");
 
 });
