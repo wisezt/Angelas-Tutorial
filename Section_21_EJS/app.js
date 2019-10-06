ccb5b1bfc0db14c33d4b96caf5ad3595dee532ea
Ting <wisezt@hotmail.com>
2019-10-06 17:30:50 -0500

    Section 21 ejs test successed

--- Section_21_EJS/app.js
+++ Section_21_EJS/app.js
@@ -5,11 +5,33 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// add ejs
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+
+
 app.get("/", function(req, res){
 
-  res.send("Hello");
+  //res.send("Hello");
+  var today = new Date(0);
+  var currentDay = today.getDay();
+  var day = "";
 
-  
+
+  if ( currentDay === 6  || currentDay === 0 ){
+    day = "Weekend";
+
+
+  } else {
+    day = "Weekday";
+
+
+  }
+
+  // views fold is default
+  res.render( "list", {theDay: day} );
+
 
 
 
