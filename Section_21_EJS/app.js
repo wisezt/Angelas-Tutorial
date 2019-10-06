d4503a33165f915831758e4d94e62854214b2780
Ting <wisezt@hotmail.com>
2019-10-06 18:14:10 -0500

    Section 21 format date

--- Section_21_EJS/app.js
+++ Section_21_EJS/app.js
@@ -11,26 +11,65 @@ app.set('view engine', 'ejs');
 
 
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
 
   //res.send("Hello");
-  var today = new Date(0);
-  var currentDay = today.getDay();
-  var day = "";
+  var today = new Date();
 
-
-  if ( currentDay === 6  || currentDay === 0 ){
-    day = "Weekend";
-
-
-  } else {
-    day = "Weekday";
-
-
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
   }
 
+var day = today.toLocaleDateString("en-US", options);
+console.log(day);
+
+
+
+
+
+  //
+  // var currentDay = today.getDay();
+  // var day = "";
+  //
+  //
+  //
+  // switch (currentDay) {
+  //   case 1:
+  //     day = "Monday";
+  //   break;
+  //   case 2:
+  //     day = "Tuesday";
+  //   break;
+  //   case 3:
+  //     day = "Webnesday";
+  //   break;
+  //   case 4:
+  //     day = "Thursday";
+  //   break;
+  //   case 5:
+  //     day = "Frieday";
+  //   break;
+  //   case 6:
+  //     day = "Saturday";
+  //   break;
+  //   case 0:
+  //     day = "Sunday";
+  //   break;
+  //   defualt:
+  //   console.log("Error: current day is equal to: " +  currentDay);
+  //
+  // }
+
+
+
+
+
   // views fold is default
-  res.render( "list", {theDay: day} );
+  res.render("list", {
+    theDay: day
+  });
 
 
 
@@ -41,7 +80,7 @@ app.get("/", function(req, res){
 });
 
 
-app.listen(3000, function(){
+app.listen(3000, function() {
 
   console.log("Server started on port 3000");
 
