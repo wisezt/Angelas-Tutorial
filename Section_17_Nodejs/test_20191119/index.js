6895d0d41e97b24495a72b8df2a66cb529af3d86
Ting <wisezt@hotmail.com>
2019-11-19 19:39:31 -0500

    'test'

---
+++ Section_17_Nodejs/test_20191119/index.js
@@ -0,0 +1,11 @@
+//jshint esversion:6
+
+//const fs = require("fs");
+
+const superheroes = require('superheroes');
+
+var my_superheroe_name = superheroes.random();
+
+console.log(my_superheroe_name);
+
+console.log("test");
